4bad0097db4fb0ff8c65c05f2b143ec4bbbf6a10
Mason Wong <masonjw1@uci.edu>
2022-11-05 11:28:09 -0700

    testing api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
+import requests
 app = Flask(__name__)
-
 @app.route("/")
-def hello_world():
+def get_parameters():
+    print("123")
     return "<p>Hello, World!</p>"
+    response = requests.get("https://api.peterportal.org/rest/v0/schedule/soc?term=2018%20Fall&department=COMPSCI&courseNumber=161")
+    response.json()
