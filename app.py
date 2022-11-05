5fb4677afae07894638925305cb145b4c442ece5
Mason Wong <masonjw1@uci.edu>
2022-11-05 14:53:22 -0700

    finished parameters

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# imports
+
 from flask import Flask
 from flask import request
 import requests
 import json
 app = Flask(__name__)
+
+# gets all the departments
 @app.route("/departments")
 
 def all_departments():
@@ -16,6 +20,7 @@ def all_departments():
     json_data = json.dumps(dep_dict)
     return json_data
 
+# gets all the terms from 2014 to 2023
 @app.route("/terms")
 
 def all_terms():
@@ -30,33 +35,37 @@ def all_terms():
     year_list.reverse()
     json_data = json.dumps(year_list)
     return json_data
+
+# Gets the json of classes that fit the parameters
 @app.route("/parameters")
-# http://127.0.0.1:5000/parameters?term=2022Fall&department=ICS&sectionCodes=12345&courseTitle=31A
 
 def get_parameters():
+    # the year in term has to be separated from the quarter with a %20 sign
     term = request.args.get('term')
     department = request.args.get('department')
     section_code = request.args.get('sectionCodes')
     course_title = request.args.get('courseTitle')
     search_parameters = {'Term': term, 'Department': department, 'Section Code': section_code, 'Course Title': course_title}
-    if term != "None":
+    string1 = ''
+    print(term)
+    if term is not None:
         term_get = "term=" + str(term)
-    else:
-        term_get = ""
-    if department != "None":
-        department_get = "department=" + str(department)
-    else:
-        department_get = ""
-    if section_code != "None":
-        section_code_get = "sectionCodes=" + str(section_code)
-    else:
-        section_code_get = ""
-    if course_title != "None":
-        course_title_get = "courseTitle=" + str(course_title)
-    else:
-        course_title_get = ""
-    string1 = term_get + " " + department_get + " " + section_code_get + " " + course_title_get
-    string2 = string1.replace(" ", "&")
+        string1 += term_get
+
+    if department is not None:
+        department_get = "&department=" + str(department)
+        string1 += department_get
+
+    if section_code is not None:
+        section_code_get = "&sectionCodes=" + str(section_code)
+        string1 += section_code_get
+
+    if course_title is not None:
+        course_title_get = "&courseTitle=" + str(course_title)
+        string1 += course_title_get
+
+    print(string1)
+    string2 = "https://api.peterportal.org/rest/v0/schedule/soc?" + string1
+    response2 = requests.get(string2)
     print(string2)
-    response2 = requests.get("https://api.peterportal.org/rest/v0/schedule/soc?" + string2)
     return response2.json()
